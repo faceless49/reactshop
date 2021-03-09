4817f4ec623a1f874b9d93fbd01d70a6bf2b240e
Egor K <digital.zeus@mail.ru>
2021-03-09 16:28:15 +0300

    install browseroute

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import Header from "./pages/header/Header";
 import CategoryList from "./pages/mainPage/category/CategoryList";
 import Hero from "./pages/mainPage/hero/Hero";
 import SearchForm from "./pages/mainPage/searchForm/SearchForm";
-import Card from './components/card/Card'
-import backpack from './assets/images/popularSlider/backpack.png'
+import Card from "./components/card/Card";
+import backpack from "./assets/images/popularSlider/backpack.png";
+import { BrowserRouter } from "react-router-dom";
+import MainSlider from "./pages/mainPage/mainSlider/MainSlider";
+import AdvertCard from "./components/advertCard/AdvertCard";
+import AdvertBlock from "./pages/mainPage/advertBlock/AdvertBlock";
 
 const App = () => {
-    return (
-    <div className="App">
-
-    </div>
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Hero/>
+        <section className="category">
+          <SearchForm text1="Поиск по  номеру" text2="Поиск по марке" text3="Поиск по названию товара" placeholder="Введите марку "/>
+          <CategoryList />
+        </section>
+        <AdvertBlock/>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 export default App;
