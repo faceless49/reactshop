b89adef0e501827054f0e895a624eb1734cece10
Egor K <digital.zeus@mail.ru>
2021-03-20 14:18:04 +0300

    make headerbottom

--- src/pages/header/headerBottom/HeaderBottom.js
+++ src/pages/header/headerBottom/HeaderBottom.js
@@ -4,14 +4,14 @@ import s from "./HeaderBottom.module.scss";
 export function HeaderBottom (props) {
   return (
     <div className={s.header_bottom}>
-      <NavLink to={'/'}>{props.linkName}Квадроциклы</NavLink>
-      <NavLink to={'/'}>{props.linkName1}Катера</NavLink>
-      <NavLink to={'/'}>{props.linkName2}Гидроциклы</NavLink>
-      <NavLink to={'/'}>{props.linkName3}Лодки</NavLink>
-      <NavLink to={'/'}>{props.linkName4}Вездеходы</NavLink>
-      <NavLink to={'/'}>{props.linkName5}Снегоходы</NavLink>
-      <NavLink to={'/'}>{props.linkName6}Двигатели</NavLink>
-      <NavLink to={'/'}>{props.linkName7}Запчасти</NavLink>
+      <NavLink className={s.header_link}to={'/'}>{props.linkName}Квадроциклы</NavLink>
+      <NavLink className={s.header_link}to={'/'}>{props.linkName1}Катера</NavLink>
+      <NavLink className={s.header_link}to={'/'}>{props.linkName2}Гидроциклы</NavLink>
+      <NavLink className={s.header_link}to={'/'}>{props.linkName3}Лодки</NavLink>
+      <NavLink className={s.header_link}to={'/'}>{props.linkName4}Вездеходы</NavLink>
+      <NavLink className={s.header_link}to={'/'}>{props.linkName5}Снегоходы</NavLink>
+      <NavLink className={s.header_link}to={'/'}>{props.linkName6}Двигатели</NavLink>
+      <NavLink className={s.header_link}to={'/'}>{props.linkName7}Запчасти</NavLink>
     </div>
   );
 }
